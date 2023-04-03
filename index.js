7dba071daa6cb34cbe87f6a9577c35e62f6d5659
Ulises <ulises.argueta95@gmail.com>
2023-04-03 13:16:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(arr, name) {
+    return arr.filter(driver => {
+      return driver.toUpperCase().includes(name.toUpperCase());
+    });
+  };
+
+function fuzzyMatch(arr2, name2) {
+    const driverMatch =[];
+    for (const driver of arr2) {
+      if(driver.slice(0,2) === name2) {
+        driverMatch.push(driver)
+      }
+    } return driverMatch
+}
+
+function matchName(arr3, name3){
+  const nameMatch = [];
+  for (const driver of arr3){
+    if (driver.name === name3)
+      nameMatch.push(driver)
+  } return nameMatch
+}
